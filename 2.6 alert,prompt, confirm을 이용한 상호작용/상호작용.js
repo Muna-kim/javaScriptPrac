1115c31ed6c748cf060e73f2dec1362eed037eb2
Muna-kim <63827788+Muna-kim@users.noreply.github.com>
2021-01-06 14:06:31 +0900

    alert,prompt,comfirm 개념 이해

---
+++ 2.6 alert,prompt, confirm을 이용한 상호작용/상호작용.js
@@ -0,0 +1,10 @@
+// 메세지가 있는 작은창 : 모달 창(modal window)
+// 모달 : 단어의 페이지의 나머지 부분과 상호작용이 불가능하다는 의미
+alert("Hello");
+
+// prompt : 두개의 인수를 받는다.
+let age = prompt('나이를 입력해 주세요.',100);
+alert(`당신의 나이는 ${age}살 입니다.`)
+
+let isBoss = confirm("당신이 주인인가요?");
+alert (isBoss);
